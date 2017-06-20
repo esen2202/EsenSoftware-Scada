b8dccc39fe843918ce318a5db034a7b1fa44a438
erkan esen <erkanesen2202@hotmail.com>
2017-06-21 00:11:59 +0300

    Interface 2

---
+++ EldorST18.wpf/Classes/Converters1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EldorST18.wpf.Classes
+{
+    public class Converters1
+
+    {
+    }
+}
--- EldorST18.wpf/MainWindow.xaml.cs
+++ EldorST18.wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EldorST18.wpf.UC.Report;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,23 @@ namespace EldorST18.wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        private System.Windows.Threading.DispatcherTimer saat = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
 
+            saat.Interval = TimeSpan.FromSeconds(1);
+            saat.IsEnabled = true;
+            saat.Tick += timer_say;
         }
 
+        private void timer_say(object sender, EventArgs e)
+        {
+            DateTime simdi = DateTime.Now;
+            lb_time.Content = simdi.ToLongTimeString();
+            lb_date.Content = simdi.ToLongDateString();
+        }
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
 
@@ -68,34 +78,33 @@ namespace EldorST18.wpf
             }
         }
 
-
         private void btn_addUserShow_Click(object sender, RoutedEventArgs e)
         {
-            ContentUserControl(gd_content, new UserAdd());
+            ContentUserControl(wp_content, new UserAdd());
         }
 
         private void btn_entryUserShow_Click(object sender, RoutedEventArgs e)
         {
-            ContentUserControl(gd_content, new UserEntry());
+            ContentUserControl(wp_content, new UserEntry());
         }
 
         private void btn_editUserShow_Click(object sender, RoutedEventArgs e)
         {
-            ContentUserControl(gd_content, new UserEdit());
+            ContentUserControl(wp_content, new UserEdit());
         }
 
         /// <summary>
         /// Call User Control in Grid
         /// </summary>
-        /// <param name="grid">Grid</param>
+        /// <param name="panel">Grid</param>
         /// <param name="uc">UserControl</param>
-        private void ContentUserControl(Grid grid, UserControl uc)
+        private void ContentUserControl(WrapPanel panel, UserControl uc)
         {
-            if (grid.Children.Count > 0)
+            if (panel.Children.Count > 0)
             {
-                grid.Children.Clear();
+                panel.Children.Clear();
             }
-            grid.Children.Add(uc);
+            panel.Children.Add(uc);
         }
 
         private void img_extendMenu_MouseDown(object sender, MouseButtonEventArgs e)
@@ -111,6 +120,11 @@ namespace EldorST18.wpf
             {
                 gc_sideMenu.Width = gl80;
             }
+        }
+
+        private void mbtn_reporting_Click(object sender, RoutedEventArgs e)
+        {
+            ContentUserControl(wp_content, new Reporting());
         }
     }
 }
